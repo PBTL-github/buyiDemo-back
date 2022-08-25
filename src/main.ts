1f6f1b8617476acf83dd92f21d4b26b769bc320a
PBTL <huajian@corpmail.com>
2022-08-25 15:26:23 +0800

    updata:

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,3 @@
-import { AppDataSource } from "./orm/data-source";
-import { User } from "./orm/entity/User";
-
-// AppDataSource.initialize()
-//     .then(async () => {
-//         const userRe = AppDataSource.getRepository(User);
-//         const user = new User();
-//         user.username = "admin";
-//         user.password = "123456";
-//         await userRe.save(user);
-//         const a = await userRe.find();
-//         console.log(a);
-//     })
-//     .catch((error) => console.log(error));
 import app from "./server";
 
 app.listen(3000, () => {
